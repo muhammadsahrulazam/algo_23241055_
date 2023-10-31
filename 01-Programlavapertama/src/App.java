dec5d973b216de1682efc49c911cce248ebb169c
muhammadsahrulazam <145996641+muhammadsahrulazam@users.noreply.github.com>
2023-10-31 13:21:19 +0800

    31 oktober 2023

---
+++ 01-Programlavapertama/src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       // program biodata mahasiswa
+       // deklarasi variable
+       String nama;
+       String nim;
+       String Tgl_lahir;
+       String alamat;
+       float berat_badan;
+       float tinggi_badan;
+
+       // mengisi variable
+       nama = "sahrul";
+       nim = "23241055";
+       Tgl_lahir = "03 november 2004,";
+       alamat = "pajangan mantang";
+       berat_badan = 50.5f;
+       tinggi_badan =181.5f;
+
+       // cetak biodata
+       System.out.println("================");
+       System.out.println("nama : "+nama);
+       System.out.println("nim : "+nim);
+       System.out.println("tanggal lahir : "+Tgl_lahir);
+       System.out.println("alamat : "+alamat);
+       System.out.println("berat badan : "+berat_badan);
+       System.out.println("tinggi badan : "+tinggi_badan);
+       System.out.println("================");
+       
+
+
+
+    
+}
